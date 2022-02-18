06702afb668039c09460645a29ad4ee043b5af26
weltitob <weltitob@gmail.com>
2022-02-18 10:36:47 +0100

    Sicherung style vor dem headerpush

--- root/js/javascript.js
+++ root/js/javascript.js
@@ -1,4 +1,3 @@
-
 //IMPORTANT NOTE https://developers.google.com/identity/sign-in/web
 
 function onSignIn(googleUser) {
@@ -16,7 +15,21 @@ var id_token = googleUser.getAuthResponse().id_token;
 console.log("ID Token: " + id_token);
 }
 
+//Replace Text function
+// Array of words 
+var helloworld = 'printhelloworld'
+var words = ['print("hello world!")', 'cool', 'amazing', 'wtf', 'warum geht das nicht'];
+var char = ['0', '1']
+// Function that executes every 2000 milliseconds 
+var t = setInterval(function() { 
 
-function textrotate{
-    
-}
+  // Random number generator 
+  var randomNumber = Math.round(Math.random() * (char.length-1)); 
+
+  // Change the word in the span for a random one in the array of words 
+  $('#change-content').html(words[randomNumber]); 
+}, 2000);
+
+//stepstextupdate
+var bodytext = ['']
+
