678aff975c32f919c63bb3b3e221b6db5dad53ec
weltitob <weltitob@gmail.com>
2022-03-13 21:38:40 +0100

    dassada

--- root/js/javascript.js
+++ root/js/javascript.js
@@ -1,26 +1,4 @@
 /**
- * function to get googleUser data
- * @param {*} googleUser 
- * not yet implemented
- */
-
-function onSignIn(googleUser) {
-var profile = googleUser.getBasicProfile();
-console.log("ID: " + profile.getId()); 
-console.log('Full Name: ' + profile.getName());
-console.log('Given Name: ' + profile.getGivenName());
-console.log('Family Name: ' + profile.getFamilyName());
-console.log("Image URL: " + profile.getImageUrl());
-console.log("Email: " + profile.getEmail());
-
-/**
- * id which gets passed to backend
- */
-var id_token = googleUser.getAuthResponse().id_token;
-console.log("ID Token: " + id_token);
-}
-
-/**
  * change active steps function
  * gets html elements through doc selectors and changes them accordingly
  */
